3fc13e0382f0de49c2b2d00a22bee932f5237b99
Devin <ddcast@gmail.com>
2014-03-17 20:03:53 -0400

    wip

--- public/scripts/roll.js
+++ public/scripts/roll.js
@@ -1,26 +1,65 @@
-$(function(){
-	// Reference to setInterval so we can clear it
-	var rolling = null;
-	// A way to speed up the rolling
-	var timesClicked = 0;
+function DiceClass() {
+	// Data store
+	this.data = {
+		// Reference to setInterval so we can clear it
+		rolling: false,
+		// A way to speed up the rolling
+		timeMultiplier: 0
+	};
+}
 
-	$('#links').on('click', '#rolling', function() {
-		timesClicked = timesClicked + 1;
-		if(rolling) {
-			clearInterval(rolling);
-		}
-		var memberList = $('.member-cell');
-		rolling = setInterval(function() { 
-			var randomIndex = Math.floor((Math.random()*memberList.length) + 1);
-			var $memberImg = $(memberList[randomIndex]).find('img');
-			$('#winner').find('img').prop("src", $memberImg.prop('src'));
-			$('#winner').find('img').prop("title", $memberImg.prop('title'));
-			$('#winner').find('h1').text('Winner is ' + $memberImg.prop('title'));
-		}, 100/timesClicked);
-	});
+DiceClass.prototype.set = function set(name, value) {
+	this.data[name] = value;
+};
 
-	$('#links').on('click', '#stop', function() {
+DiceClass.prototype.get = function set(name) {
+	var value = this.data[name];
+
+	return value;
+};
+
+DiceClass.prototype.stopRoll = function stopRoll() {
+	var rolling = this.get('rolling');
+	clearInterval(rolling);
+	this.set('timeMultiplier', 0);
+	this.set('rolling', false);
+};
+
+DiceClass.prototype.time = function time(increment) {
+	this.set('timeMultiplier', this.get('timeMultiplier') + increment);
+
+	var value = this.get('timeMultiplier');
+
+	return value;
+};
+
+DiceClass.prototype.startRoll = function startRoll() {
+	var timeMultiplier = this.time(1);
+	var rolling = this.get('rolling');
+	if(rolling) {
 		clearInterval(rolling);
-		timesClicked = 0;
+	}
+	var memberList = $('.member-cell');
+	this.set('rolling', setInterval(function rollingInterval() { 
+		var randomIndex = Math.floor((Math.random()*memberList.length) + 1);
+		var $memberImg = $(memberList[randomIndex]).find('img');
+		$('#winner').find('img').prop("src", $memberImg.prop('src'));
+		$('#winner').find('img').prop("title", $memberImg.prop('title'));
+		$('#winner').find('h1').text('Winner is ' + $memberImg.prop('title'));
+	}, 100/timeMultiplier));
+};
+
+$(function(){
+	// Instance of dice
+	var dice = new DiceClass();
+
+	$('#winner').on('click', '.winner__wrapper', function rollDiceHandler() {
+		var rolling = dice.get('rolling');
+
+		if (rolling) {
+			dice.stopRoll();
+		} else {
+			dice.startRoll();
+		} 
 	});
 });
