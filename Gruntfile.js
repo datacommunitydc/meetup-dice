22848c0c0c3c3d9bc96d5119f0889f51bbd0a055
Devin <ddcast@gmail.com>
2014-03-30 22:57:03 -0400

    adding source maps

--- Gruntfile.js
+++ Gruntfile.js
@@ -21,6 +21,18 @@ module.exports = function(grunt) {
             cwd: 'bower_components/animate.css/',
             src: ['animate.css'],
             dest: 'public/stylesheets/'
+          },
+          {
+            expand: true,
+            cwd: 'bower_components/platform/',
+            src: ['platform.js.map'],
+            dest: 'public/scripts/'
+          },
+          {
+            expand: true,
+            cwd: 'bower_components/polymer/',
+            src: ['polymer.js.map'],
+            dest: 'public/scripts/'
           }
         ]
       }
